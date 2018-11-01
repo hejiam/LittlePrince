a3291261631a5f1044c943f54c1afa3a9dc849eb
hejiam <hejiam55@163.com>
2018-11-01 21:14:19 +0800

    commit one

---
+++ learnCplusplus/cmake-build-debug/CMakeFiles/learnCplusplus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnCplusplus.dir/Do_while_loop.cpp.o"
+  "learnCplusplus.pdb"
+  "learnCplusplus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learnCplusplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
